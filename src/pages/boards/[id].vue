68451ab997d1c71971ae10126c0a9a98d037fc87
MerlinAlex(HJP) <merlinalex@qq.com>
2022-07-15 01:01:23 +0800

    feat: add apollo requests

--- src/pages/boards/[id].vue
+++ src/pages/boards/[id].vue
@@ -2,6 +2,10 @@
 import { ref, toRefs } from 'vue'
 import BoardDragAndDrop from '../../components/BoardDragAndDrop.vue'
 
+import type { Task } from '@/types'
+import { v4 as uuidv4 } from 'uuid'
+import AppPageHeading from '../../components/AppPageHeading.vue'
+
 const props = defineProps({
   id: String
 })
@@ -9,28 +13,44 @@ const props = defineProps({
 const { id: boardId } = toRefs(props)
 
 const board = ref({
-  id: boardId.value,
+  id: boardId?.value || '1',
   title: "Let's have an amazing time at Vue.js forge!!",
   order: JSON.stringify([{ id: '1', title: 'backlog', taskIds: ['1', '2'] }])
 })
 
-const tasks = ref([
+const tasks = ref<Partial<Task>[]>([
   { id: '1', title: 'Code like mad people' },
   { id: '2', title: 'Push clean code' }
 ])
 
-const updateBoard = (b) => (board.value = b)
+async function addTask(task: Task) {
+  return new Promise((resolve, reject) => {
+    const taskWithTheId = {
+      ...task,
+      id: uuidv4()
+    }
+    tasks.value.push(taskWithTheId)
+    resolve(taskWithTheId)
+  })
+}
+
+const updateBoard = (b) => {
+  board.value = b
+}
+
+const deleteBoardIfConfirmed = () => {
+  console.log('delete board')
+}
 </script>
 
 <template>
   <div>
-    <board-drag-and-drop :board="board" :tasks="tasks" @update="updateBoard" />
+    <AppPageHeading>
+      {{ board.title }}
+    </AppPageHeading>
+    <BoardMenu :board="board" @deleteBoard="deleteBoardIfConfirmed" />
 
-    <details>
-      <pre>
-        {{ board }}
-      </pre>
-    </details>
+    <BoardDragAndDrop :tasks="tasks" :board="board" @update="updateBoard" :addTask="addTask" />
   </div>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { fileURLToPath, URL } from "url";
-
-import { defineConfig } from "vite";
+import { fileURLToPath, URL } from 'url'
+import { defineConfig } from 'vite'
+import graphql from '@rollup/plugin-graphql'
 
 // plugins
-import vue from "@vitejs/plugin-vue";
-import Pages from "vite-plugin-pages";
-import Components from "unplugin-vue-components/vite";
+import vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Pages(), Components()],
+  plugins: [vue(), Pages(), Components(), graphql()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-});
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
