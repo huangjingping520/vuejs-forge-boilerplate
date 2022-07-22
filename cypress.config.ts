edd73679273fcccbd4e33c53abf095f1f7522843
MerlinAlex(HJP) <merlinalex@qq.com>
2022-07-22 15:14:50 +0800

    chore: volar reproduction

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,5 +4,11 @@ export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:4173'
+  },
+  component: {
+    devServer: {
+      framework: 'vue',
+      bundler: 'vite'
+    }
   }
 })
